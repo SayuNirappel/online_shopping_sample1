f572091ee239e423f9381d52b9e3e4a197903404
SayuNirappel <sayunirappel@gmail.com>
2025-02-19 02:43:55 +0530

    Home Page completed

--- lib/view/home_page/home_page.dart
+++ lib/view/home_page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:online_shopping/view/main_page/main_page.dart';
 
 void main() {}
 
@@ -14,6 +15,42 @@ class HomePage extends StatelessWidget {
                 image: NetworkImage(
                     "https://images.pexels.com/photos/4694499/pexels-photo-4694499.jpeg?auto=compress&cs=tinysrgb&w=600"),
                 fit: BoxFit.cover)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.all(20),
+              child: TextButton(
+                  style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Colors.black),
+                      shape: WidgetStatePropertyAll(BeveledRectangleBorder())),
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (context) => MainPage()),
+                        (route) => false);
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Get Started",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Icon(
+                        Icons.arrow_forward_outlined,
+                        color: Colors.white,
+                        size: 30,
+                      )
+                    ],
+                  )),
+            ),
+          ],
+        ),
       ),
     );
   }
