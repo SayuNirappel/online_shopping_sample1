a6fa0e062c79b27866dd39a9b97b686c84af02dd
SayuNirappel <sayunirappel@gmail.com>
2025-02-21 10:38:47 +0530

    image button

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -3,71 +3,79 @@ import 'package:online_shopping/view/home_screen/tabs/tab_all.dart';
 
 void main() {}
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Container(
-          decoration: BoxDecoration(
-              color: Colors.grey.shade200,
-              borderRadius: BorderRadius.circular(10)),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SizedBox(
-                width: 20,
-              ),
-              Icon(
-                Icons.search,
-                size: 30,
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Expanded(
-                child: TextField(
-                  maxLines: 1,
-                  decoration: InputDecoration(
-                    hintText: "Search Anything",
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Container(
+            decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: BorderRadius.circular(10)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Icon(
+                  Icons.search,
+                  size: 30,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: TextField(
+                    maxLines: 1,
+                    decoration: InputDecoration(
+                      hintText: "Search Anything",
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ),
-        actions: [
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.black, borderRadius: BorderRadius.circular(10)),
-            child: Icon(
-              Icons.filter_list,
-              color: Colors.white,
-              size: 40,
+              ],
             ),
           ),
-          SizedBox(
-            width: 20,
-          )
-        ],
-        bottom: TabBar(tabAlignment: TabAlignment.start, tabs: [
-          Tab(
-            text: "All",
-          ),
-          Tab(
-            text: "Men",
-          ),
-          Tab(
-            text: "Women",
-          ),
-          Tab(
-            text: "Kids",
-          ),
-        ]),
+          actions: [
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.black, borderRadius: BorderRadius.circular(10)),
+              child: Icon(
+                Icons.filter_list,
+                color: Colors.white,
+                size: 40,
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            )
+          ],
+          bottom: TabBar(tabAlignment: TabAlignment.fill, tabs: [
+            Tab(
+              text: "All",
+            ),
+            Tab(
+              text: "Men",
+            ),
+            Tab(
+              text: "Women",
+            ),
+            Tab(
+              text: "Kids",
+            ),
+          ]),
+        ),
+        body: TabBarView(children: [TabAll(), TabAll(), TabAll(), TabAll()]),
       ),
-      body: TabBarView(children: [TabAll()]),
     );
   }
 }
--- lib/view/home_screen/tabs/tab_all.dart
+++ lib/view/home_screen/tabs/tab_all.dart
@@ -7,14 +7,35 @@ class TabAll extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Colors.pinkAccent),
-      child: Center(
-        child: Text(
-          "Caht Tab",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
+    return Scaffold(
+      body: GridView.builder(
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          itemCount: 30,
+          itemBuilder: (context, int index) {
+            return Container(
+              margin: EdgeInsets.all(20),
+              decoration: BoxDecoration(color: Colors.grey.shade100),
+              child: Column(
+                //mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextButton(
+                      onPressed: () {},
+                      child: Image(
+                        image: NetworkImage(
+                            "https://images.pexels.com/photos/447570/pexels-photo-447570.jpeg?auto=compress&cs=tinysrgb&w=600"),
+                        fit: BoxFit.cover,
+                      )),
+                  Text(
+                    "Regular Suit",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Text("PKR 1,190")
+                ],
+              ),
+            );
+          }),
     );
   }
 }
